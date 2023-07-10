52b5475da1d3bc7fb4cf0fa3f279729fe7405d8d
OroGeek <ahmia.ahmed@gmail.com>
2023-07-10 13:58:16 +0100

    init printing

---
+++ Printing/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Printing;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Printing/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+
+namespace Printing;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		var path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+		PdfDocument pdf = new PdfDocument(new PdfWriter(path + "/facture.pdf"));
+		Document document = new Document(pdf);
+		string line = "Hello World";
+		document.Add(new Paragraph(line));
+		document.Close();
+
+		DisplayAlert("Success", "Document Created", "OK"); 
+    }
+}
+
